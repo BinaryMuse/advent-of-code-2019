d9c0474f9984cb15b4ce581dc97ea25fc7d0e36a
Michelle Tilley <michelle@michelletilley.net>
2019-12-06 14:46:46 -0800

    Day 4 part 2

--- rust/src/runners/day04.rs
+++ rust/src/runners/day04.rs
@@ -2,15 +2,20 @@ pub fn run(input: String, _args: &[String]) {
   let nums: Vec<u32> = input.split('-').map(|s| s.parse::<u32>()).collect::<Result<_, _>>().unwrap();
   let first = nums.first().unwrap();
   let last = nums.last().unwrap();
-  let valid_count = (*first..=*last).filter(|num| {
-    is_valid(format!("{}", num).as_str())
-  }).count();
-  println!("Part 1: {} valid passwords", valid_count);
+  let valid = (*first..=*last).filter(|num| {
+    is_valid(&num.to_string(), false)
+  });
+  println!("Part 1: {} valid passwords", valid.count());
+
+  let extra_valid = (*first..=*last).filter(|num| {
+    is_valid(&num.to_string(), true)
+  });
+  println!("Part 2: {} valid passwords", extra_valid.count());
 }
 
-fn is_valid(s: &str) -> bool {
-  let mut found_double = false;
+fn is_valid(s: &str, enforce_runs: bool) -> bool {
   let mut last_num = None;
+  let mut runs: Vec<(u32, usize)> = vec![];
 
   for byte in s.bytes() {
     // There has got to be a better way to do this
@@ -19,21 +24,40 @@ fn is_valid(s: &str) -> bool {
     match last_num {
       Some(digit) => {
         last_num = Some(num);
-        if num == digit { found_double = true };
         if num < digit { return false };
+        if num == digit {
+          // same as last number; increment the run
+          let run = runs.pop().unwrap();
+          runs.push((run.0, run.1 + 1));
+        } else {
+          // different; add a new run
+          runs.push((num, 1));
+        }
       },
       None => {
         last_num = Some(num);
+        runs.push((num, 1));
       }
     }
   };
 
-  found_double
+  if enforce_runs {
+    runs.iter().any(|(_, count)| *count == 2)
+  } else {
+    runs.iter().any(|(_, count)| *count >= 2)
+  }
 }
 
 #[test]
 fn test_validity() {
-  assert_eq!(is_valid("111111"), true);
-  assert_eq!(is_valid("223450"), false);
-  assert_eq!(is_valid("123789"), false);
+  assert_eq!(is_valid("111111", false), true);
+  assert_eq!(is_valid("223450", false), false);
+  assert_eq!(is_valid("123789", false), false);
+}
+
+#[test]
+fn test_validity_pt_2() {
+  assert_eq!(is_valid("112233", true), true);
+  assert_eq!(is_valid("123444", true), false);
+  assert_eq!(is_valid("111122", true), true);
 }
